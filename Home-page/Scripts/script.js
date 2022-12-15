819c679d1b9bf68f25a2aba7b89acfc1f75ba129
senzu199702 <v.plentauskas@gmail.com>
2022-12-15 23:07:20 +0200

    header done

--- Home-page/Scripts/script.js
+++ Home-page/Scripts/script.js
@@ -1,11 +1,17 @@
 const menuBtn = document.querySelector('.menu-toggle');
+const navMenu = document.querySelector(".nav");
+
 let menuOpen = false;
+
 menuBtn.addEventListener('click', () => {
     if (!menuOpen) {
         menuBtn.classList.add('open');
+        navMenu.classList.toggle("active");
         menuOpen = true;
     } else {
         menuBtn.classList.remove('open');
         menuOpen = false;
+        navMenu.classList.toggle("active");
+
     }
 })
