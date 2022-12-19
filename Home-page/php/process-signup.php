369d8ec661ad9cb5c22fe4e7cff9d80bb037f7ce
senzu199702 <v.plentauskas@gmail.com>
2022-12-19 17:05:33 +0200

    update

--- Home-page/php/process-signup.php
+++ Home-page/php/process-signup.php
@@ -1,4 +1,5 @@
 <?php
+$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
 
 if(empty($_POST['username'])){
     die("Name is required");
@@ -21,6 +22,33 @@ if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
 if ( ! preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain at least one number");
 }
+$mysqli = include "./database/db.php";
+$sql = "INSERT INTO user (username , password_hash , email)
+VALUES (?,?,?)";
+
+$stmt = $mysqli->stmt_init();
+
+if ( ! $stmt->prepare($sql)) {
+    die("SQL error: " . $mysqli->error);
+}
+
+$stmt->bind_param("sss",$_POST['username'],$password_hash,$_POST['email'],);
+
+mysqli_report(MYSQLI_REPORT_OFF);
+if ($stmt->execute()) {
+
+    header("Location: signup-success.html");
+    exit;
+    
+} else {
+    
+    if ($mysqli->errno === 1062) {
+       echo("email already taken");
+    } else {
+        echo($mysqli->error . " " . $mysqli->errno);
+    }
+}
+echo $mysqli->error;
 
 
 
