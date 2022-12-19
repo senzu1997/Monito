ccf58aa17ee36edb8601579f5b9ffcbca6e8cba1
senzu199702 <v.plentauskas@gmail.com>
2022-12-19 15:05:49 +0200

    register validation

--- Home-page/php/process-signup.php
+++ Home-page/php/process-signup.php
@@ -1,9 +1,26 @@
 <?php
-print_r($_POST);
 
+if(empty($_POST['username'])){
+    die("Name is required");
+}
 
+if(! filter_var($_POST["email"],FILTER_VALIDATE_EMAIL)) {
+    die('Valid email is required');
+}
+if ($_POST['password'] !== $_POST['repeatPassword']) {
+    die("Password adress does not match");
+}
+if (strlen($_POST['password']) <8) {
+    die("Password must be at least 8 characters");
+}
 
+if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
+    die("Password must contain at least one letter");
+}
 
+if ( ! preg_match("/[0-9]/", $_POST["password"])) {
+    die("Password must contain at least one number");
+}
 
 
 
