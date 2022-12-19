1885876a2d8777b620e67f2b6da360be1ba5fc1f
senzu199702 <v.plentauskas@gmail.com>
2022-12-19 22:31:36 +0200

    working on login

--- Home-page/php/login.php
+++ Home-page/php/login.php
@@ -10,7 +10,11 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 
                    if ($user) {
                     if (password_verify($_POST['password'], $user['password_hash'])) {
-                        die('Login successful');
+                        session_start();
+                        $_SESSION['user_id'] = $user['id'];
+
+                        header('Location: index.php');
+                        exit;
                     };
                     
                 }
