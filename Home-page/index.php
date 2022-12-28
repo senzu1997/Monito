c917cf5cb00b224c5f78fbc7bbf5213bb65086c7
senzu199702 <v.plentauskas@gmail.com>
2022-12-28 15:57:13 +0200

    Card start

--- Home-page/index.php
+++ Home-page/index.php
@@ -72,6 +72,20 @@ if (isset($_SESSION["user_id"])) {
             </div>
         </div>
     </header>
+    <section>
+        <div class="section-header">
+            <p>Whats new?</p>
+            <div class="header-bottom">
+                <h3>Take a Look At Some Of Our Pets</h3>
+                <button>View more
+                    <img src="./Images/Icons/arrow-icon.png" alt="arrow-icon">
+                </button>
+            </div>
+        </div>
+        <div class="card-container">
+
+        </div>
+    </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
---
+++ Home-page/php/models/Card.php
@@ -0,0 +1,16 @@
+<?php
+class Card
+{
+    public $title;
+    public $gender;
+    public $age;
+    public $price;
+
+
+
+
+
+
+
+}
+?>
